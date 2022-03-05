038c1efed75680e7d35c0fbe5c691dad20e48b47
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2022-03-04 18:17:36 -0800

    created Header component and styled it using styled components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import './App.css';
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
-  return <Header/>
+  return (
+    <Wrapper>
+      <Header />
+      <Main>
+        <MainContent style = {{ width: '100vw' }}>
+          <TodoContent>
+            <Title>Dashboard</Title>
+            <Greeting>Good Morning, Isaac</Greeting>
+            {[<h2>Cooking</h2>, <h2>Reading</h2>]}
+          </TodoContent>
+        </MainContent>
+      </Main>
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  background-color: #18181f;
+  min-height: 100vh;
+  min-width: 100vw;
+  color: #eee;
+`;
+const Main = styled.div`
+  display: flex;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  transition: 0.3s;
+`;
+
+const TodoContent = styled.div``;
+
+const Title = styled.div``;
+
+const Greeting = styled.div``;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-    return <h2>KILL KILL KILL</h2>
-}
+  return (
+    <Wrapper>
+      <HeaderItem>
+        <i className="fas fa-bars" />
 
-export default Header
+      </HeaderItem>
+
+      <HeaderItem>
+        <i className="fas fa-border-all" />
+        <span>Dashboard</span>
+      </HeaderItem>
+
+      <HeaderItem>
+        <i className="fas fa-images" />
+        <span>Collections</span>
+      </HeaderItem>
+
+      <Placeholder />
+
+      <HeaderItem>
+        <Profile>
+            <img
+                src = 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.18169-9/28168403_1791775667541393_4345244685546332976_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=BVaDd9PqSQUAX8f_2V_&_nc_ht=scontent-lax3-2.xx&oh=00_AT-9jw0WjRKRoyY4prBO_Y_F8Tl6qh3ZHHfvcSLGtpYuJQ&oe=623DE3CC'
+                alt = ''
+            />
+        </Profile>
+      </HeaderItem>
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 70px;
+    align-items: center;
+    background-color: #20212d;
+    padding: 0 20px;
+    -webkit-box-shadow: 0px 4px 15px #121212;
+    box-shadow: 0px 4px 15px #121212;
+    position: sticky;
+    top: 0;
+`;
+const HeaderItem = styled.div`
+    color: #eee;
+    padding: 10px 16px;
+    border-radius: 4px;
+
+    span{
+        margin-left: 10px;
+        font-weight:  500;
+    }
+
+    &:hover{
+        background-color: #18181f;
+        transition: 0.3s;
+        cursor: pointer;
+    }
+`;
+const Placeholder = styled.div`
+    flex: 1;
+`;
+const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    
+    img {
+        height: 30px;
+        border-radius: 50%;
+    }
+`;
